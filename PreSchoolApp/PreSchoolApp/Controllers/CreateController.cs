78f2d8d63535c0a93dc90f93fba8fae336cf65d6
Jakob Larsson <yjlarsson@gmail.com>
2017-12-05 11:48:25 +0100

    Post-metoder till login- och create-controllers

--- PreSchoolApp/PreSchoolApp/Controllers/CreateController.cs
+++ PreSchoolApp/PreSchoolApp/Controllers/CreateController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using PreSchoolApp.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,36 @@ namespace PreSchoolApp.Controllers
 {
     public class CreateController : Controller
     {
+        UserManager<IdentityUser> userManager;
+        SignInManager<IdentityUser> signInManager;
+        RoleManager<IdentityRole> roleManager;
+
+        public CreateController
+            (
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            RoleManager<IdentityRole> roleManager
+            )
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Create(CreateUserVM createUserVM)
+        {
+            var result =
+                await userManager.CreateAsync(new IdentityUser(createUserVM.UserName), createUserVM.PassWord);
+
+            return RedirectToAction(nameof(TeacherController));
         }
     }
 }
