3214c376c9c3c65d193da9277c0a6db7db328e28
Jakob Larsson <yjlarsson@gmail.com>
2017-12-05 11:23:06 +0100

    Login Post

--- PreSchoolApp/PreSchoolApp/Controllers/LoginController.cs
+++ PreSchoolApp/PreSchoolApp/Controllers/LoginController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using PreSchoolApp.Models.ViewModels;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -36,6 +37,27 @@ namespace PreSchoolApp.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(HomeLoginVM loginVM)
+        {
+            if (!ModelState.IsValid)
+                return View(loginVM);
+
+            //Här skapas cookien
+            var result = await signInManager.PasswordSignInAsync(
+            loginVM.UserName, loginVM.Password, false, false);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(
+                    nameof(HomeLoginVM.UserName), "Felaktigt namn eller lösenord"); //lägger in felmeddelande                
+                return View(loginVM);
+            }
+
+            return RedirectToAction(nameof(Search));
         }
     }
 }
