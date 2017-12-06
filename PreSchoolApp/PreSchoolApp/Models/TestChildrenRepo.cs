e12835e0f083c2c4f51d73b6a7f68c7acb0502f2
Jakob Larsson <yjlarsson@gmail.com>
2017-12-06 11:37:59 +0100

    Mitt i testrepo-bygge

---
+++ PreSchoolApp/PreSchoolApp/Models/TestChildrenRepo.cs
@@ -0,0 +1,33 @@
+﻿using PreSchoolApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreSchoolApp.Models
+{
+    public class TestChildrenRepo
+    {        
+        //static List<Schedules> schedules = TestSchedulesRepo.GetSchedule();
+        static List<Children> customers = new List<Children>
+        {
+            new Children{Id = 1, FirstName = "Sara", LastName = "Andersson", IsPresent = false, UnitsId = 1},
+            new Children{Id = 2, FirstName = "Kalle", LastName = "Johansson", IsPresent = false, UnitsId = 1},
+            new Children{Id = 3, FirstName = "Olle", LastName = "Lundmark", IsPresent = false, UnitsId = 1},
+            new Children{Id = 4, FirstName = "Anna", LastName = "Marklund", IsPresent = false, UnitsId = 1},
+            new Children{Id = 5, FirstName = "Pelle", LastName = "Eriksson", IsPresent = false, UnitsId = 1}
+        };
+
+
+        //public CustomersIndexVM[] GetAllCustomers()
+        //{
+        //    return customers
+        //        .Select(c => new CustomersIndexVM
+        //        {
+        //            CompanyName = c.CompanyName,
+        //            IsVip = c.CompanyName.StartsWith("S")
+        //        })
+        //        .ToArray();
+        //}        
+    }
+}
---
+++ PreSchoolApp/PreSchoolApp/Models/TestSchedulesRepo.cs
@@ -0,0 +1,52 @@
+﻿using PreSchoolApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreSchoolApp.Models
+{
+
+    public class TestSchedulesRepo
+    {
+        static TimeSpan dropOff = new TimeSpan(8, 00, 00);
+        static TimeSpan pickUp = new TimeSpan(16, 00, 00);
+
+        static List<Schedules> schedules = new List<Schedules>
+        {
+            new Schedules{Id = 1, ChildrenId = 1, Dropoff = dropOff, PickUp = pickUp, Weekdays = "Monday"},
+            new Schedules{Id = 1, ChildrenId = 1, Dropoff = dropOff, PickUp = pickUp, Weekdays = "Tuesday"},
+            new Schedules{Id = 1, ChildrenId = 1, Dropoff = dropOff, PickUp = pickUp, Weekdays = "Wednesd"},
+            new Schedules{Id = 1, ChildrenId = 1, Dropoff = dropOff, PickUp = pickUp, Weekdays = "Thursda"},
+            new Schedules{Id = 1, ChildrenId = 1, Dropoff = dropOff, PickUp = pickUp, Weekdays = "Friday"},
+            new Schedules{Id = 2, ChildrenId = 2, Dropoff = dropOff, PickUp = pickUp, Weekdays = "Monday"},
+            new Schedules{Id = 2, ChildrenId = 2, Dropoff = dropOff, PickUp = pickUp, Weekdays = "Tuesday"},
+            new Schedules{Id = 2, ChildrenId = 2, Dropoff = dropOff, PickUp = pickUp, Weekdays = "Wednesd"},
+            new Schedules{Id = 2, ChildrenId = 2, Dropoff = dropOff, PickUp = pickUp, Weekdays = "Thursda"},
+            new Schedules{Id = 2, ChildrenId = 2, Dropoff = dropOff, PickUp = pickUp, Weekdays = "Friday"},
+        };
+
+        static public List<Schedules> GetSchedule()
+        {
+            return schedules;
+        }
+
+        //static public List<Schedules> GetScheduleForChild(int id)
+        //{
+        //    List<Schedules> childSchedule = 
+
+        //    return schedules;
+        //}
+
+        //public CustomersIndexVM[] GetAllCustomers()
+        //{
+        //    return customers
+        //        .Select(c => new CustomersIndexVM
+        //        {
+        //            CompanyName = c.CompanyName,
+        //            IsVip = c.CompanyName.StartsWith("S")
+        //        })
+        //        .ToArray();
+        //}
+    }
+}
