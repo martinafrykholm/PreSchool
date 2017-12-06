4fff6ba93cad0982d478187eaf8ef09941b664c6
Anafs91 <32542320+Anafs91@users.noreply.github.com>
2017-12-06 09:38:17 +0100

    Lagt till ändringar för Childrenkolumnen i SQL.

--- PreSchoolApp/PreSchoolApp/Models/Entities/Children.cs
+++ PreSchoolApp/PreSchoolApp/Models/Entities/Children.cs
@@ -15,6 +15,9 @@ namespace PreSchoolApp.Models.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int? UnitsId { get; set; }
+        public bool IsPresent { get; set; }
+        public bool? IsIll { get; set; }
+        public int? MinLate { get; set; }
 
         public Units Units { get; set; }
         public ICollection<C2p> C2p { get; set; }
