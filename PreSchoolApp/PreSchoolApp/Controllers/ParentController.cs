af3469d91680c79336c6ba368854fb9328551850
Anafs91 <32542320+Anafs91@users.noreply.github.com>
2017-12-08 15:08:26 +0100

    Slängde in repository i ParentController

--- PreSchoolApp/PreSchoolApp/Controllers/ParentController.cs
+++ PreSchoolApp/PreSchoolApp/Controllers/ParentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PreSchoolApp.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,18 @@ namespace PreSchoolApp.Controllers
 {
     public class ParentController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        PreSchoolDBRepository repository;
+
+        public ParentController(PreSchoolDBRepository repository)
         {
-            return View();
+            this.repository = repository;
         }
+
+        //// GET: /<controller>/
+        ////public IActionResult Index()
+        //{
+        //    //var model = repository.GetYourChild();
+        //    //return View(model);
+        //}
     }
 }
