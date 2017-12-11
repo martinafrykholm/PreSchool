9c6207045af0f5cb731a06145ede2b19a8d024d1
Jakob Larsson <yjlarsson@gmail.com>
2017-12-11 16:18:42 +0100

    Utils-klass

---
+++ PreSchoolApp/PreSchoolApp/Models/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreSchoolApp.Models
+{
+    public class Utils
+    {
+        public static TimeSpan[] GetAllTimes()
+        {
+            List<TimeSpan> allTimeSpans = new List<TimeSpan>();
+            int hour = 8;
+            int minute = 0;
+
+            for (int i = 0; i < 55; i++)
+            {
+                TimeSpan allTimes = new TimeSpan(hour, minute, 00);
+                allTimeSpans.Add(allTimes);
+                minute = minute + 10;
+                if (minute == 60)
+                {
+                    hour++;
+                    minute = 0;
+                }
+            }
+
+            return allTimeSpans.ToArray();
+        }
+    }
+}
