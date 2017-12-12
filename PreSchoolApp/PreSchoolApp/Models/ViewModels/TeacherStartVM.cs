09057c4465dfd3fdd3cbc123f4de2516666a572c
Anafs91 <32542320+Anafs91@users.noreply.github.com>
2017-12-12 12:56:02 +0100

    TAgit bort knappar i rapportvyn, samt lagt till MinLate; int; prop på TeacherStartChildItemVM

--- PreSchoolApp/PreSchoolApp/Models/ViewModels/TeacherStartVM.cs
+++ PreSchoolApp/PreSchoolApp/Models/ViewModels/TeacherStartVM.cs
@@ -24,6 +24,7 @@ namespace PreSchoolApp.Models.ViewModels
         public TimeSpan PickupTime { get; set; }
         public bool IsPresent { get; set; }
         public bool IsActive { get; set; }
+        public int MinLate { get; set; }
     }
 }
 
