a84f47bcb3a5be7008233e5b200ca047f6efbefa
Martina <21558@student.hhs.se>
2017-12-12 15:08:05 +0100

    Rättade till felet

--- PreSchoolApp/PreSchoolApp/wwwroot/Scripts/CreateIndex.js
+++ PreSchoolApp/PreSchoolApp/wwwroot/Scripts/CreateIndex.js
@@ -1,1 +1,16 @@
-﻿
+﻿$(document).ready(function () {
+
+
+    $(".ButtonStyleLarge").hover(function () {
+
+        $(this).css('background-color', 'rgba(249,110,175, 0.2)')
+
+    }, function () {
+
+
+        $(this).css('background-color', '')
+    });
+
+
+
+})
