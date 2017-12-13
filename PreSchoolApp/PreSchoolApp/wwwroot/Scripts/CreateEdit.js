ab650e94e01436d1601cd2b010f3eabc2ad6bbf5
Anafs91 <32542320+Anafs91@users.noreply.github.com>
2017-12-13 16:37:40 +0100

    Lagt in dagens datum samt lagt till semikolon i JS.

--- PreSchoolApp/PreSchoolApp/wwwroot/Scripts/CreateEdit.js
+++ PreSchoolApp/PreSchoolApp/wwwroot/Scripts/CreateEdit.js
@@ -1,18 +1,18 @@
 ﻿$(document).ready(function () {
 
-    
+
     $("#bodyDiv").fadeIn(1000);
 
     $(".ButtonStyleLarge").hover(function () {
 
-        $(this).css('background-color', 'rgba(249,110,175, 0.2)')
+        $(this).css('background-color', 'rgba(249,110,175, 0.2)');
 
     }, function () {
 
 
-        $(this).css('background-color', '')
+        $(this).css('background-color', '');
     });
 
 
 
-})
+});
--- PreSchoolApp/PreSchoolApp/wwwroot/Scripts/ParentIndex.js
+++ PreSchoolApp/PreSchoolApp/wwwroot/Scripts/ParentIndex.js
@@ -3,23 +3,29 @@
 
     $(".ButtonStyleLargeAutoSize").hover(function () {
 
-        $(this).css('background-color', 'rgba(249,110,175, 0.2)')
+        $(this).css('background-color', 'rgba(249,110,175, 0.2)');
 
     }, function () {
 
 
-        $(this).css('background-color', '')
-    })
+        $(this).css('background-color', '');
+    });
 
 
     $(".ButtonStyleLargeAutoSize").hover(function () {
 
-        $(this).css('background-color', 'rgba(249,110,175, 0.2)')
+        $(this).css('background-color', 'rgba(249,110,175, 0.2)');
 
     }, function () {
 
 
-        $(this).css('background-color', '')
-    })
+        $(this).css('background-color', '');
+    });
+
+    var d = new Date();
+        var weekday = new Array("Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag");
+        $("p#GetDay").append(weekday[d.getDay()]);
+    
 });
+
 
