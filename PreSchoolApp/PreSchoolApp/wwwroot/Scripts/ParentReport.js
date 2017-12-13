26caf4c7e9e16fca9c0da8dcda3f8e07ba2146d0
Martina <21558@student.hhs.se>
2017-12-13 10:42:43 +0100

    Försökt fixa javascript på reportsidan men det gick sådär

---
+++ PreSchoolApp/PreSchoolApp/wwwroot/Scripts/ParentReport.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+
+    var name = document.getElementById("FirstName").innerText;
+    var delay = document.getElementsByClassName("ButtonStyleLargeAutoSizeLate").value;
+
+    $(".ButtonStyleLargeAutoSizeLate").click(function () {
+
+        alert("Okej. Du blir försenad med " + delay);
+
+    });
+
+    $(".ButtonStyleLargeAutoSizeLate").hover(function () {
+
+        $(this).css('background-color', 'rgba(249,110,175, 0.2)')
+
+    }, function () {
+
+
+        $(this).css('background-color', '')
+        })
+
+
+    $(".ButtonStyleLargeAutoSizeIll").click(function () {
+
+        alert("Okej." + name +" är sjuk idag");
+
+    }, function () {
+
+
+        $(this).css('background-color', '')
+    })
+
+    $(".ButtonStyleLargeAutoSizeIll").hover(function () {
+
+        $(this).css('background-color', 'rgba(249,110,175, 0.2)')
+
+    }, function () {
+
+
+        $(this).css('background-color', '')
+    })
+
+});
